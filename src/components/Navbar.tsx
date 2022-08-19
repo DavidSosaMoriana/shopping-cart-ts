6f2f97339eb9a71d8faa7d7c2c29a62d86249aa5
DavidSosaMoriana <sosamorianadav@gmail.com>
2022-08-19 08:46:07 +0100

    :zap: cart quantity logic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import { useShoppingCart } from '../context/ShoppingCartContext';
 
 function Navbar() {
+    const { openCart, cartQuantity } = useShoppingCart();
     return (
         <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
             <Container>
@@ -16,7 +18,8 @@ function Navbar() {
                         About
                     </Nav.Link>
                 </Nav>
-                <Button
+                {cartQuantity > 0 && (<Button
+                    onClick={openCart}
                     style={{
                         width: '3rem',
                         height: '3rem',
@@ -44,9 +47,9 @@ function Navbar() {
                             transform: 'translate(25%, 25%)',
                         }}
                     >
-                        1
+                        {cartQuantity}
                     </div>
-                </Button>
+                </Button>)}
             </Container>
         </NavbarBs>
     );
